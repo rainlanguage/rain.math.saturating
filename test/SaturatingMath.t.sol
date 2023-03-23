c2ddedd31206a0f41b59b3c037f576e144fe74f7
thedavidmeister <thedavidmeister@gmail.com>
2023-03-23 12:39:06 +0400

    tests

--- test/SaturatingMath.t.sol
+++ test/SaturatingMath.t.sol
@@ -5,7 +5,6 @@ import "forge-std/Test.sol";
 import "../src/SaturatingMath.sol";
 
 contract SaturatingMathTest is Test {
-
     function testNotOverflowAdd(uint256 a_, uint256 b_) public {
         unchecked {
             uint256 c_ = a_ + b_;
@@ -33,4 +32,36 @@ contract SaturatingMathTest is Test {
         assertEq(a_ - b_, SaturatingMath.saturatingSub(a_, b_));
     }
 
+    function testSaturateSub(uint256 a_, uint256 b_) public {
+        unchecked {
+            uint256 c_ = a_ - b_;
+            vm.assume(c_ > a_);
+
+            assertEq(0, SaturatingMath.saturatingSub(a_, b_));
+        }
+    }
+
+    function testNotOverflowMul(uint256 a_, uint256 b_) public {
+        unchecked {
+            uint256 c_ = a_ * b_;
+            vm.assume(a_ != 0);
+            vm.assume(c_ / a_ == b_);
+        }
+
+        assertEq(a_ * b_, SaturatingMath.saturatingMul(a_, b_));
+    }
+
+    function testSaturateMul(uint256 a_, uint256 b_) public {
+        unchecked {
+            uint256 c_ = a_ * b_;
+            vm.assume(a_ != 0);
+            vm.assume(c_ / a_ != b_);
+        }
+
+        assertEq(type(uint256).max, SaturatingMath.saturatingMul(a_, b_));
+    }
+
+    function testAZeroMul(uint256 b_) public {
+        assertEq(0, SaturatingMath.saturatingMul(0, b_));
+    }
 }
