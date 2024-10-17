e7a5fb56e21a57c38a60daaecd4c37ae818efde8
thedavidmeister <thedavidmeister@gmail.com>
2024-10-17 16:28:56 +0400

    lint

--- src/lib/LibSaturatingMath.sol
+++ src/lib/LibSaturatingMath.sol
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
 pragma solidity ^0.8.18;
 
-/// @title SaturatingMath
+/// @title LibSaturatingMath
 /// @notice Sometimes we neither want math operations to error nor wrap around
 /// on an overflow or underflow. In the case of transferring assets an error
 /// may cause assets to be locked in an irretrievable state within the erroring
@@ -12,40 +12,40 @@ pragma solidity ^0.8.18;
 /// Ideally there are no calculation mistakes, but in guarding against bugs it
 /// may be safer pragmatically to saturate arithmatic at the numeric bounds.
 /// Note that saturating div is not supported because 0/0 is undefined.
-library SaturatingMath {
+library LibSaturatingMath {
     /// Saturating addition.
-    /// @param a_ First term.
-    /// @param b_ Second term.
-    /// @return Minimum of a_ + b_ and max uint256.
-    function saturatingAdd(uint256 a_, uint256 b_) internal pure returns (uint256) {
+    /// @param a First term.
+    /// @param b Second term.
+    /// @return Minimum of a + b and max uint256.
+    function saturatingAdd(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
-            uint256 c_ = a_ + b_;
-            return c_ < a_ ? type(uint256).max : c_;
+            uint256 c = a + b;
+            return c < a ? type(uint256).max : c;
         }
     }
 
     /// Saturating subtraction.
-    /// @param a_ Minuend.
-    /// @param b_ Subtrahend.
-    /// @return Maximum of a_ - b_ and 0.
-    function saturatingSub(uint256 a_, uint256 b_) internal pure returns (uint256) {
+    /// @param a Minuend.
+    /// @param b Subtrahend.
+    /// @return Maximum of a - b and 0.
+    function saturatingSub(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
-            return a_ > b_ ? a_ - b_ : 0;
+            return a > b ? a - b : 0;
         }
     }
 
     /// Saturating multiplication.
-    /// @param a_ First term.
-    /// @param b_ Second term.
-    /// @return Minimum of a_ * b_ and max uint256.
-    function saturatingMul(uint256 a_, uint256 b_) internal pure returns (uint256) {
+    /// @param a First term.
+    /// @param b Second term.
+    /// @return Minimum of a * b and max uint256.
+    function saturatingMul(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
             // Gas optimization: this is cheaper than requiring 'a' not being
             // zero, but the benefit is lost if 'b' is also tested.
             // https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-            if (a_ == 0) return 0;
-            uint256 c_ = a_ * b_;
-            return c_ / a_ != b_ ? type(uint256).max : c_;
+            if (a == 0) return 0;
+            uint256 c = a * b;
+            return c / a != b ? type(uint256).max : c;
         }
     }
 }
---
+++ test/src/lib/SaturatingMath.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity =0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {LibSaturatingMath} from "src/lib/LibSaturatingMath.sol";
+
+contract SaturatingMathTest is Test {
+    function testNotOverflowAdd(uint256 a, uint256 b) public {
+        unchecked {
+            uint256 c = a + b;
+            vm.assume(c >= a);
+        }
+
+        assertEq(a + b, LibSaturatingMath.saturatingAdd(a, b));
+    }
+
+    function testSaturateAdd(uint256 a, uint256 b) public {
+        unchecked {
+            uint256 c = a + b;
+            vm.assume(c < a);
+        }
+
+        assertEq(type(uint256).max, LibSaturatingMath.saturatingAdd(a, b));
+    }
+
+    function testNotUnderflowSub(uint256 a, uint256 b) public {
+        unchecked {
+            uint256 c = a - b;
+            vm.assume(c <= a);
+        }
+
+        assertEq(a - b, LibSaturatingMath.saturatingSub(a, b));
+    }
+
+    function testSaturateSub(uint256 a, uint256 b) public {
+        unchecked {
+            uint256 c = a - b;
+            vm.assume(c > a);
+
+            assertEq(0, LibSaturatingMath.saturatingSub(a, b));
+        }
+    }
+
+    function testNotOverflowMul(uint256 a, uint256 b) public {
+        unchecked {
+            uint256 c = a * b;
+            vm.assume(a != 0);
+            vm.assume(c / a == b);
+        }
+
+        assertEq(a * b, LibSaturatingMath.saturatingMul(a, b));
+    }
+
+    function testSaturateMul(uint256 a, uint256 b) public {
+        unchecked {
+            uint256 c = a * b;
+            vm.assume(a != 0);
+            vm.assume(c / a != b);
+        }
+
+        assertEq(type(uint256).max, LibSaturatingMath.saturatingMul(a, b));
+    }
+
+    function testAZeroMul(uint256 b) public {
+        assertEq(0, LibSaturatingMath.saturatingMul(0, b));
+    }
+}
